83f060f19793ed85c05bfd535b367b4664d38cd8
Christopher N <141798586+christopher-webdev@users.noreply.github.com>
2025-03-05 08:28:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-main/',  // Ensure this matches your GitHub Pages repo name
+  base: './',  // Ensure this matches your GitHub Pages repo name
   plugins: [react()]
 })
